2fe87989cc25c4d56bde63379a09a8c64a61a2c0
Karesz <karcsibalazs@gmail.com>
2023-04-01 13:52:47 +0200

    HAZI05 fix

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -62,46 +62,65 @@ class KNNClassifier:
 
         x_train, y_train = features[:train_size], labels[:train_size]
         x_test, y_test = features[train_size:], labels[train_size:]
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
+        self.x_train = x_train.reset_index(drop=True)
+        self.y_train = y_train.reset_index(drop=True)
+        self.x_test = x_test.reset_index(drop=True)
+        self.y_test = y_test.reset_index(drop=True)
         #return (x_train,y_train,x_test,y_test)
 
     def euclidean(self,element_of_x:pd.core.frame.Series) -> pd.core.frame.DataFrame:
-        eu = self.x_train- element_of_x
+        eu = self.x_train-element_of_x
         eu = eu ** 2
-        eu = eu.sum()
+        eu = eu.sum(axis=1)
         eu = eu ** (1/2)
         return eu
 
         #return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
 
-    def predict(self,x_test:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    def predict(self, x_test):
         labels_pred = []
         for i in range(len(x_test)):
-            distances = pd.Series(KNNClassifier.euclidean(self, self.x_test.iloc[i]))
-            #distances = np.array(sorted(zip(distances,self.y_train)))
-            label_pred = distances.nsmallest(self.k)
-            labels_pred.append(label_pred)
+            distances = KNNClassifier.euclidean(self,x_test.iloc[i]).tolist()
+            for j in range(len(distances)):
+                distances[j] = (distances[j],self.y_train[j])
+            distances.sort(key=lambda a: a[0])
+            distances = distances[:self.k]
+            for h in range(len(distances)):
+                distances[h] = distances[h][1]
+            labels_pred.append(mode(distances, keepdims=False).mode.item())
+            #print(mode(distances))
 
-        self.y_preds = pd.DataFrame(label_pred)
-        #return np.array(labels_pred,dtype=np.int32)
+
+        #print(labels_pred)
+        self.y_preds = pd.Series(labels_pred)
 
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
 
-    def plot_confusion_matrix(self):
+    def confusion_matrix(self):
             conf_matrix = confusion_matrix(self.y_test,self.y_preds)
-            sns.heatmap(conf_matrix,annot=True)
+            #sns.heatmap(conf_matrix,annot=True)
+            return conf_matrix
 
-    def which_k(self)->Tuple[int,float]:
+    def best_k(self)->Tuple[int,float]:
         results = list()
-        for i in range(1, 21):
+        for i in range(1, 20):
             self.k = i
             KNNClassifier.predict(self,self.x_test)
             acc = round(KNNClassifier.accuracy(self), 2)
             results.append((i, acc))
 
         return max(results, key=lambda item: item[1])
+
+#x, y = KNNClassifier.load_csv("diabetes.csv")
+
+#classifier = KNNClassifier(4, 0.2)
+
+#classifier.train_test_split(x, y)
+#classifier.predict(classifier.x_test)
+
+#print(classifier.accuracy())
+#classifier.confusion_matrix()
+
+#print(classifier.best_k())
