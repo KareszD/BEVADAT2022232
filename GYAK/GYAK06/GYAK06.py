231bd410bc8d227e31c59d58dc61c360553dbdd1
Karesz <karcsibalazs@gmail.com>
2023-04-05 20:11:13 +0200

    GYAK06 fix

--- GYAK/GYAK06/GYAK06.py
+++ GYAK/GYAK06/GYAK06.py
@@ -16,7 +16,7 @@ X = data.iloc[:,:-1].values
 Y = data.iloc[:,-1].values.reshape(-1,1)
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=.2,random_state=41)
 
-classifier = DecisionTreeClassifier(min_sample_split=3,max_depth=3)
+classifier = DecisionTreeClassifier(min_samples_split=3,max_depth=3)
 classifier.fit(X_train,Y_train)
 
 Y_pred = classifier.predict(X_test)
