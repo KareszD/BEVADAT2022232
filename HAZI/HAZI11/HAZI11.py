58948415695a94db5c77a2a1f2b6a8c91a0f4d2e
Karesz <karcsibalazs@gmail.com>
2023-05-11 10:44:32 +0200

    HAZI11 fix

--- HAZI/HAZI11/HAZI11.py
+++ HAZI/HAZI11/HAZI11.py
@@ -12,7 +12,7 @@ függvény neve: cifar100_data
 '''
 
 def cifar100_data():
-    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar100.load_data()
     train_images, test_images = train_images / 255.0, test_images / 255.0
     return train_images, train_labels, test_images, test_labels
 
